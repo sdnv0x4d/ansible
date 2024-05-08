eb9288454f3671153b13ee4dba60c7b7d5e45c98
sdnv <sdnvgp@gmail.com>
2024-05-08 12:29:43 +0500

    Added dns records configuration for connection over VPN

--- roles/srv_setup/files/scripts/add_vpn_client.sh
+++ roles/srv_setup/files/scripts/add_vpn_client.sh
@@ -4,7 +4,7 @@ ip_mt="" # IP of Mikrotik - VPN GW
 COLOR0="echo -e \\033[1;32m" # Green color
 COLOR1="echo -e \\033[0;39m" # Standart color
 username="" # SSH user
-
+domain=example.com
 
 clients_path=$1
 clients_list="$(ls $1)"
@@ -21,7 +21,7 @@ set_new_ip(){
   local host=$1
   local hostpass=$2
   nextip $(ssh ${username}@$ip_mt -o "StrictHostKeyChecking no" -p 22 "/ppp/secret/print proplist=remote-address terse" | grep -oP 'address=\K.*' | sort -k 1n | tail -n 1)   # Take biggest IP from PPP/secret list and make next ip
-  ssh ${username}@$ip_mt -o "StrictHostKeyChecking no" -p 22 "/ppp/secret/add name=$host service=l2tp profile=profile1 remote-address=$NEXT_IP password=$hostpass"            # Add new host with new IP and new pass
+  ssh ${username}@$ip_mt -o "StrictHostKeyChecking no" -p 22 "/ppp/secret/add name=$host service=l2tp profile=profile1 remote-address=$NEXT_IP password=$hostpass; /ip/dns/static/add name=$host.$domain type=A address=$NEXT_IP"            # Add new host with new IP and new pass
   echo $NEXT_IP > $clients_path/$host
 }
 
